62c77aa178be3cb8b913c58ac0a4898ad95c0052
ErrorSteve <liguanchenohg@163.com>
2024-07-12 18:28:50 +0800

    initial commit

---
+++ mall-product/src/main/java/com/caoyao/mallproduct/MallProductApplication.java
@@ -0,0 +1,13 @@
+package com.caoyao.mallproduct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallProductApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallProductApplication.class, args);
+    }
+
+}
